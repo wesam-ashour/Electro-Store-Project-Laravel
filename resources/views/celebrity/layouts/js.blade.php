1963a2b9d1c7cd7de1711c4bb526f7deaeab9b4e
Eng.Wesam <wesam@EngWesams-MacBook-Pro.local>
2023-01-31 15:49:48 +0200

    first commit 12

--- resources/views/celebrity/layouts/js.blade.php
+++ resources/views/celebrity/layouts/js.blade.php
@@ -1,4 +1,4 @@
-<script src="http://code.jquery.com/jquery-3.4.1.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#sub_category_name').on('change', function() {
